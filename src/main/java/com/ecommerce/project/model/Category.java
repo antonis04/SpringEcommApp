b08c6056db12680027489e488f2e4371c990911a
Antek <antoni.samol@gmail.com>
2026-05-01 10:39:02 +0200

    spring lesson in progress

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -2,11 +2,14 @@ package com.ecommerce.project.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
@@ -16,6 +19,8 @@ public class Category {
         this.categoryName = categoryName;
     }
 
+    public Category() {
+    }
 
     public String getCategoryName() {
         return categoryName;
